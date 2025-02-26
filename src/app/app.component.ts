31f53357fddda2d3c13dc548ebc104d1bbadd841
Álvaro Muñoz <alvaromual13@gmail.com>
2025-02-26 14:51:00 +0100

    Actualizado todo lo demás.
    Borrado el .espect y configurado los ts para que funcione todo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { ProductFormComponent } from './components/product-form/product-form.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { TotalComponent } from './components/total/total.component';
+import { ProductFormComponent } from "./components/product-form/product-form.component";
+import { ProductListComponent } from "./components/product-list/product-list.component";
+import { TotalComponent } from "./components/total/total.component";
 import { Iproduct } from './interfaces/iproduct.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductFormComponent,ProductListComponent,TotalComponent],
+  imports: [ProductFormComponent, ProductListComponent, TotalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  productList: Iproduct[] = [
-    {title: 'balon baloncesto', price: 15, quantity: 10},
-    {title: 'zapatillas de running', price: 85, quantity: 25},
+  productsList: Iproduct[] = [
+    { title: 'Balon baloncesto', price: 64.99, quantity: 10 },
+    { title: 'Zapatillas de running', price: 140, quantity: 25 },
   ];
+
+  getDataForm(event: Iproduct) {
+    // this.productsList.push($event)
+    this.productsList = [...this.productsList, event]
+  }
 }
