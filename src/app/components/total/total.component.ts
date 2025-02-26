c48eea52ce41623c95cdd7f99675421a3bfc3ee2
alvaro-fullstack <alvaromual13@gmail.com>
2025-02-26 14:35:51 +0100

    actualizado total

--- src/app/components/total/total.component.ts
+++ src/app/components/total/total.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { IProduct } from '../../interfaces/iproduct.interface';
 
 @Component({
   selector: 'app-total',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './total.component.css'
 })
 export class TotalComponent {
+  products = input<IProduct[]>([]);
+  totalprice: number = 0;
+  totalQuantity: number = 0;
 
+  ngOnChanges() {
+    //this.totalQuantity = this.products.reduce((ac: number, product: IProduct) => ac + product.quantity, 0)
+    //console.log(this.totalQuantity)
+
+    for (let product of this.products()) {
+      this.totalprice += product.price * product.quantity
+      this.totalQuantity += product.quantity
+    }
+    console.log(this.totalprice)
+    console.log(this.totalQuantity)
+
+  }
 }
